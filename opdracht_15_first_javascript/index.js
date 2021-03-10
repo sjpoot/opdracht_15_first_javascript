cf2c5da59a896bc21d2e33baf4c1ce3e64edaafc
sjpoot <78816393+sjpoot@users.noreply.github.com>
2021-03-10 17:11:14 +0100

    Add files via upload

---
+++ opdracht_15_first_javascript/index.js
@@ -0,0 +1,23 @@
+console.log('Hello Winc Academy');
+
+let firstName = 'Bas';
+console.log(firstName);
+
+//Je kunt geen variable "if" maken want dit is een reserved keyword.//
+
+let numbers = 4 + 4;
+console.log(numbers);
+
+let strings = "4" + "4";
+console.log(strings);
+//"4" zijn strings, dus deze worden aan elkaar geplakt en wordt niet mee gerekend//
+
+let som1 = 44 + 36;
+let som2 = 44 - 36;
+let som3 = 44 * 36;
+let som4 = 44 / 36;
+let som5 = 44 % 36;
+console.log(som1, som2, som3, som4, som5);
+
+let leeftijdBas = "48";
+console.log(leeftijdBas)
